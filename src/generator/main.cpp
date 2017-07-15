1449d1b489ed15d95afbbf62853aeeb849f1a502
Steex <sot@tut.by>
2017-07-16 02:42:48 +0300

    Generator is reading its output

--- src/generator/main.cpp
+++ src/generator/main.cpp
@@ -1,7 +1,22 @@
 #include "gen_head.h"
 
+#include <iostream>
 
-int main()
+
+int main(int argc, char *argv[])
 {
+    // Read system parameters.
+    if (argc != 3)
+    {
+        std::cerr << "Bad arguments count" << std::endl;
+        return 1;
+    }
+
+    String xml_path = argv[1];
+    String out_path = argv[2];
+
+    std::cout << "xml = " << (std::string)xml_path << std::endl;
+    std::cout << "out = " << (std::string)out_path << std::endl;
+
     return 0;
 }
